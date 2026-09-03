61d461b15804bcca980c5c08f8198ad93e7a058a
Stilyan <stili4771@gmail.com>
2026-09-03 21:19:32 +0300

    added login and register validations and reworked the front-end

--- src/main/java/com/example/ChibiWallet/web/IndexController.java
+++ src/main/java/com/example/ChibiWallet/web/IndexController.java
@@ -7,10 +7,13 @@ import com.example.ChibiWallet.model.dto.user.UserRegisterRequest;
 import com.example.ChibiWallet.model.entity.user.Country;
 import com.example.ChibiWallet.model.entity.user.User;
 import com.example.ChibiWallet.service.user.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class IndexController {
@@ -31,12 +34,18 @@ public class IndexController {
 
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
-        mav.addObject("userLoginData", userLoginRequest);
+        mav.addObject("userLoginRequest", userLoginRequest);
         return mav;
     }
 
     @PostMapping("/login")
-    public ModelAndView login (@ModelAttribute UserLoginRequest userLoginRequest){
+    public ModelAndView login (@Valid UserLoginRequest userLoginRequest,BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("userLoginRequest", userLoginRequest);
+            mav.setViewName("login");
+            return mav;
+        }
         UserDto userDto = userService.login(userLoginRequest);
 
         ModelAndView mav = new ModelAndView();
@@ -59,7 +68,15 @@ public class IndexController {
     }
 
     @PostMapping("/register")
-    public ModelAndView Register(@ModelAttribute UserRegisterRequest userRegisterRequest, Model model){
+    public ModelAndView Register(@Valid UserRegisterRequest userRegisterRequest, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("userRegisterRequest",userRegisterRequest);
+            mav.addObject("countries", Country.values());
+            mav.setViewName("register");
+            return mav;
+        }
         userService.register(userRegisterRequest);
         return new ModelAndView("redirect:/login");
     }
